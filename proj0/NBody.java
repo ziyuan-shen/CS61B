724fd0716d0a99313ab8eded97a805ee6c714be7
Ziyuan Shen <ziyuan.shen@duke.edu>
2020-09-11 02:13:56 -0700

    draw background

--- proj0/NBody.java
+++ proj0/NBody.java
@@ -22,4 +22,17 @@ public class NBody {
         }
         return planets;
     }
+
+    public static void main(String[] args) {
+        double T = Double.parseDouble(args[0]);
+        double dt = Double.parseDouble(args[1]);
+        String filename = args[2];
+        Planet[] planets = readPlanets(filename);
+        double radius = readRadius(filename);
+
+        StdDraw.setScale(-radius, radius);
+        StdDraw.clear();
+        StdDraw.picture(0, 0, "images/starfield.jpg");
+        StdDraw.show();
+    }
 }
