f0e94faf1a43bd0a449900ffb194d7a9bd4490b1
Ziyuan Shen <ziyuan.shen@duke.edu>
2020-09-11 01:57:29 -0700

    add readPlanets

--- proj0/NBody.java
+++ proj0/NBody.java
@@ -5,4 +5,21 @@ public class NBody {
         double radius = in.readDouble();
         return radius;
     }
+
+    public static Planet[] readPlanets(String fileName) {
+        In in = new In(fileName);
+        int num = in.readInt();
+        double radius = in.readDouble();
+        Planet[] planets = new Planet[num];
+        for (int i = 0; i < num; i++) {
+            double xP = in.readDouble();
+            double yP = in.readDouble();
+            double xV = in.readDouble();
+            double yV = in.readDouble();
+            double m = in.readDouble();
+            String img = in.readString();
+            planets[i] = new Planet(xP, yP, xV, yV, m, img);
+        }
+        return planets;
+    }
 }
