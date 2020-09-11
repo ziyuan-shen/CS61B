edb9c024455bb0bbc696b4727f4068d9898f0168
Ziyuan Shen <ziyuan.shen@duke.edu>
2020-09-11 15:07:39 -0700

    create an animation

--- proj0/NBody.java
+++ proj0/NBody.java
@@ -29,14 +29,44 @@ public class NBody {
         String filename = args[2];
         Planet[] planets = readPlanets(filename);
         double radius = readRadius(filename);
-
+        
+        StdDraw.enableDoubleBuffering();
         StdDraw.setScale(-radius, radius);
         StdDraw.clear();
         StdDraw.picture(0, 0, "images/starfield.jpg");
         StdDraw.show();
 
+        /*
         for (int i=0; i<planets.length; i++) {
             planets[i].draw();
         }
+        */
+
+        double t;
+        
+        for (t=0; t <= T; t += dt) {
+            double[] xForces = new double[planets.length];
+            double[] yForces = new double[planets.length];
+            for (int i=0; i<planets.length; i++) {
+                xForces[i] = planets[i].calcNetForceExertedByX(planets);
+                yForces[i] = planets[i].calcNetForceExertedByY(planets);
+            }
+            for (int i=0; i<planets.length; i++) {
+                planets[i].update(dt, xForces[i], yForces[i]);
+            }
+            StdDraw.picture(0, 0, "images/starfield.jpg");
+            for (int i=0; i<planets.length; i++) {
+                planets[i].draw();
+            }
+            StdDraw.show();
+            StdDraw.pause(10);
+        }
+        StdOut.printf("%d\n", planets.length);
+        StdOut.printf("%.2e\n", radius);
+        for (int i = 0; i < planets.length; i++) {
+            StdOut.printf("%11.4e %11.4e %11.4e %11.4e %11.4e %12s\n",
+                planets[i].xxPos, planets[i].yyPos, planets[i].xxVel,
+                planets[i].yyVel, planets[i].mass, planets[i].imgFileName);   
+}
     }
 }
