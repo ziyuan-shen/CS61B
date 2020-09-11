a7ab3583d53dbdcb03162d32ac29123574a9a44d
Ziyuan Shen <ziyuan.shen@duke.edu>
2020-09-10 21:36:44 -0700

    add NBody.java

---
+++ proj0/NBody.java
@@ -0,0 +1,8 @@
+public class NBody {
+    public static double readRadius(String fileName) {
+        In in = new In(fileName);
+        int num = in.readInt();
+        double radius = in.readDouble();
+        return radius;
+    }
+}
