0dda758cbb2cb12c5105b7dc6247d6fcf304ccc4
Ziyuan Shen <ziyuan.shen@duke.edu>
2020-09-16 22:51:09 -0700

    add equalChars

--- proj1b/OffByOne.java
+++ proj1b/OffByOne.java
@@ -1,5 +1,7 @@
 public class OffByOne implements CharacterComparator {
     public boolean equalChars(char x, char y) {
-        return true;
+        int xx = (int) x;
+        int yy = (int) y;
+        return (xx - yy) == 1 || (xx - yy) == -1;
     }
 }
