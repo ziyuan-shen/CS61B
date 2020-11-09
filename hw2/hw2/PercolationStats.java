92990d3040189f3472efb5b88b7af456e539c540
Ziyuan Shen <ziyuan.shen@duke.edu>
2020-11-08 23:06:46 -0800

    add hw2 PercolationStats

--- hw2/hw2/PercolationStats.java
+++ hw2/hw2/PercolationStats.java
@@ -1,5 +1,42 @@
 package hw2;
 
-public class PercolationStats {
+import edu.princeton.cs.introcs.StdRandom;
+import edu.princeton.cs.introcs.StdStats;
 
+public class PercolationStats {
+    int T;
+    double[] threshold;
+
+    public PercolationStats(int N, int T, PercolationFactory pf) {
+        if (N <= 0) {
+            throw new java.lang.IllegalArgumentException("N too small");
+        }
+        if (T <= 0) {
+            throw new java.lang.IllegalArgumentException("T too small");
+        }
+        this.T = T;
+        threshold = new double[T];
+        for(int i = 0; i < T; i++) {
+            Percolation p = pf.make(N);
+            while (!p.percolates()) {
+                int row = StdRandom.uniform(N);
+                int col = StdRandom.uniform(N);
+                p.open(row, col);
+            }
+            threshold[i] = p.numberOfOpenSites() / (N * N);
+        }
+    }
+
+    public double mean() {
+        return StdStats.mean(threshold);
+    }
+    public double stddev() {
+        return StdStats.stddev(threshold);
+    }
+    public double confidentLow() {
+        return mean() - 1.96 * stddev() / Math.sqrt(T);
+    }
+    public double confidentHigh() {
+        return mean() + 1.96 * stddev() / Math.sqrt(T);
+    }
 }
