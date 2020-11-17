2b1c7a4514b9213cc828c8392fc32ce1ff540cec
Ziyuan Shen <ziyuan.shen@duke.edu>
2020-11-16 23:45:47 -0800

    add TestComplexOomage to hw3

--- hw3/hw3/hash/TestComplexOomage.java
+++ hw3/hw3/hash/TestComplexOomage.java
@@ -6,6 +6,7 @@ import static org.junit.Assert.assertTrue;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Arrays;
 
 public class TestComplexOomage {
 
@@ -33,18 +34,19 @@ public class TestComplexOomage {
         assertTrue(OomageTestUtility.haveNiceHashCodeSpread(oomages, 10));
     }
 
-    /* TODO: Create a list of Complex Oomages called deadlyList
-     * that shows the flaw in the hashCode function.
-     */
-    /*
     @Test
     public void testWithDeadlyParams() {
         List<Oomage> deadlyList = new ArrayList<>();
 
-        // Your code here.
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                deadlyList.add(new ComplexOomage(new ArrayList<Integer>(
+                        Arrays.asList(i, j, 1, 2, 3, 4))));
+            }
+        }
 
         assertTrue(OomageTestUtility.haveNiceHashCodeSpread(deadlyList, 10));
-    } */
+    }
 
     /** Calls tests for SimpleOomage. */
     public static void main(String[] args) {
