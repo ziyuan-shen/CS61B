3a58ded2e8771d634a79403d54f1ba014fc1ca4b
Ziyuan Shen <ziyuan.shen@duke.edu>
2020-12-30 02:04:07 -0800

    hw4 complete solver

--- hw4/hw4/puzzle/SearchNode.java
+++ hw4/hw4/puzzle/SearchNode.java
@@ -1,4 +1,17 @@
 package hw4.puzzle;
 
-public class SearchNode {
+public class SearchNode implements Comparable<SearchNode> {
+    public WorldState ws;
+    public int moves;
+    public SearchNode prev;
+
+    public int compareTo(SearchNode node) {
+        return (this.moves + this.ws.estimatedDistanceToGoal()) - (node.moves + node.ws.estimatedDistanceToGoal());
+    }
+
+    public SearchNode(WorldState ws, int moves, SearchNode prev) {
+        this.ws = ws;
+        this.moves = moves;
+        this.prev = prev;
+    }
 }
--- hw4/hw4/puzzle/Solver.java
+++ hw4/hw4/puzzle/Solver.java
@@ -1,5 +1,34 @@
 package hw4.puzzle;
 import edu.princeton.cs.algs4.MinPQ;
+import java.util.ArrayList;
 
 public class Solver {
+    private int move;
+    private ArrayList<WorldState> seq;
+
+    public Solver(WorldState initial) {
+        MinPQ<SearchNode> q = new MinPQ<SearchNode>();
+        SearchNode X = new SearchNode(initial, 0, null);
+        while (!X.ws.isGoal()) {
+            for (WorldState nei: X.ws.neighbors()) {
+                if (!(X.prev != null && nei == X.prev.ws)) {
+                    q.insert(new SearchNode(nei, X.moves + 1, X));
+                }
+            }
+            X = q.delMin();
+        }
+        move = X.moves;
+        seq = new ArrayList<WorldState>();
+        while (X != null) {
+            seq.add(0, X.ws);
+            X = X.prev;
+        }
+    }
+
+    public int moves() {
+        return move;
+    }
+    public Iterable<WorldState> solution() {
+        return seq;
+    }
 }
